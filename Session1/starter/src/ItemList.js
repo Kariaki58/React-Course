fcb69642757f745e0a5200d5b07a0b83e322a95d
kariaki58 <programmerjavascript901@gmail.com>
2024-03-10 10:01:01 +0100

    todo list

---
+++ Session1/starter/src/ItemList.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { FaTrashAlt } from 'react-icons/fa'
+
+
+const ItemList = ({item, handleCheck, handleDelete}) => {
+  return (
+    <li className='item'>
+        <input onChange={() => handleCheck(item.id)} type='checkbox' checked={item.checked}/>
+        <label style={(item.checked)? { textDecoration: 'line-through' }: null} onDoubleClick={() => handleCheck(item.id)}>{item.item}</label>
+        <FaTrashAlt onClick={() => handleDelete(item.id)} role='button' tabIndex="0"/>
+    </li>
+  )
+}
+
+export default ItemList
