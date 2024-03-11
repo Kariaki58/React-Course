321456d76e58b4393940dedb751544ba70db483d
kariaki58 <programmerjavascript901@gmail.com>
2024-03-11 23:06:08 +0100

    modifying code

--- Session1/blogapp/src/Footer.js
+++ Session1/blogapp/src/Footer.js
@@ -1,9 +1,10 @@
 import React from 'react'
 
 const Footer = () => {
+  const today = new Date()
   return (
-    <footer>
-      <h1>Footer</h1>
+    <footer className='Footer'>
+      <p>Copywright &copy; {today.getFullYear()}</p>
     </footer>
   )
 }
--- Session1/blogapp/src/PostPage.js
+++ Session1/blogapp/src/PostPage.js
@@ -1,9 +1,29 @@
 import React from 'react'
+import { useParams, Link } from 'react-router-dom'
 
-const PostPage = () => {
+const PostPage = ({ posts, handleDelete }) => {
+  const { id } = useParams()
+  const post = posts.find(post => (post.id).toString() === id)
   return (
-    <main>
-      <h1>Post page</h1>
+    <main className='PostPage'>
+      <article className='post'>
+        {post && 
+          <>
+            <h2>{post.title}</h2>
+            <p className='postDate'>{post.datetime}</p>
+            <p className='postBody'>{post.body}</p>
+            <Link to={`/edit/${post.id}`}><button className='editButton'>Edit</button></Link>
+            <button className='deleteButton' onClick={() => handleDelete(post.id)}>Delete</button>
+          </>
+        }
+        {!post && 
+        <>
+          <h2>Post Not Found</h2>
+          <p>Well, that's disappointing</p>
+          <p><Link to='/'>Visit Our HomePage</Link></p>
+        </>
+        }
+      </article>
     </main>
   )
 }
