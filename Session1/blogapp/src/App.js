0734029e4e3bfd7982a1955c3b5e3b4334c92100
kariaki58 <programmerjavascript901@gmail.com>
2024-03-12 08:13:39 +0100

    custom hook

--- Session1/blogapp/src/App.js
+++ Session1/blogapp/src/App.js
@@ -12,36 +12,24 @@ import { format } from 'date-fns'
 import api from './api/posts' // axios instance
 import EditPost from './EditPost'
 import useWindowSize from './hooks/useWindowSize'
+import useAxiosFetch from './hooks/useAxiosFetch'
 
 
 function App() {
-  const[posts, setPosts] = useState([])
+  const [posts, setPosts] = useState([])
   const [search, setSearch] = useState('');
-  const[searchResults, setSearchResults] = useState([]);
+  const [searchResults, setSearchResults] = useState([]);
   const [postTitle, setPostTitle] = useState('')
   const [postBody, setPostBody] = useState('')
   const [editTitle, setEditTitle] = useState('')
   const [editBody, setEditBody] = useState('')
   const { width } = useWindowSize()
+  const { data, fetchError, isLoading } = useAxiosFetch('http://localhost:3500/posts')
   const history = useNavigate()
 
   useEffect(() => {
-    const fetchPosts = async () => {
-      try {
-        const response = await api.get('/posts');
-        setPosts(response.data)
-      } catch (err) {
-        if (err.response) {
-          console.log(err.response.data);
-          console.log(err.response.status);
-          console.log(err.response.headers);
-        } else {
-          console.log(`Error: ${err.message}`);
-        }
-      }
-    }
-    fetchPosts();
-  }, [])
+    setPosts(data);
+  }, [data])
 
   useEffect(() => {
     const filteredResults = posts.filter(post => (
@@ -101,7 +89,10 @@ function App() {
       <Header title="React JS Blog" width={width}/>
       <Nav search={search} setSearch={setSearch}/>
       <Routes>
-        <Route path='/' element={<Home posts={searchResults}/>} />
+        <Route path='/' element={<Home 
+            posts={searchResults}
+            fetchError={fetchError}
+            isLoading={isLoading} />} />
         <Route path='/post' element={<NewPost 
             handleSubmit={handleSubmit}
             postTitle={postTitle}
